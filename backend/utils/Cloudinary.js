8526e6da981adeb5f32a32d01feb72b5b2197843
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-12-17 20:18:01 +0545

    feat: implement Cloudinary functionality for image upload

---
+++ backend/utils/Cloudinary.js
@@ -0,0 +1,21 @@
+import { v2 as Cloudinary } from 'cloudinary'
+import fs from 'fs'
+Cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadCloudinary = async (localpath) => {
+    try {
+        if (!localpath) return null
+        const res = await Cloudinary.uploader.upload(localpath, {
+            resource_type: 'auto'
+        })
+        return res
+    } catch (error) {
+        console.log(error)
+        fs.unlinkSync(localpath)
+
+    }
+}
